62ec0bb5a2064296a5ddb88e3c7e4e3d38b2b38b
BenjaminFourmaux <benjamin.fourmauxb@gmail.com>
2023-02-15 11:37:35 +0100

    PoC (it's work ! cool now dev !)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TeamsDeck")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Microsoft Teams plugin for Elgato Streamdeck")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TeamsDeck")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Benjamin Fourmaux -- Beruet,  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
